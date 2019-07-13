890a80a92614017efc2c971e19e221c4fde20286
smk87 <30999133+smk87@users.noreply.github.com>
2019-07-13 21:15:35 +0600

    Realotrs model added.

--- listings/models.py
+++ listings/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from realtors.models import Realtor
+from datetime import datetime
 
 # Create your models here.
 
 
 class Listing(models.Model):
-    realtor = models.ForeignKey(realtor, on_delete=models.DO_NOTHING)
+    realtor = models.ForeignKey(Realtor, on_delete=models.DO_NOTHING)
     tittle = models.CharField(max_length=200)
     address = models.CharField(max_length=200)
     city = models.CharField(max_length=200)
@@ -28,7 +29,7 @@ class Listing(models.Model):
     photo_5 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     photo_6 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     is_published = models.BooleanField(default=True)
-    list_date = models.DateTimeField(default=datetime.now, blank=True)
+    list_date = models.DateTimeField(default=datetime.now(), blank=True)
 
     def __str__(self):
         return self.tittle  # Set the visible name in the admin area
