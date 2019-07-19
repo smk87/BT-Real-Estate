080009cad1ea2c149004a3365580fba77b616084
smk87 <30999133+smk87@users.noreply.github.com>
2019-07-19 15:16:45 +0600

    Realtor and Listing app added to Admin area.

--- btre/settings.py
+++ btre/settings.py
@@ -120,9 +120,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+
 # Store static files into one folder
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'btre/static')
 ]  # Fetch static files from whole project
+
+
+# Media Folder Setiings
+# Store static files into one folder
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
